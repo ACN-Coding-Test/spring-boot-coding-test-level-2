a03e99071ae3269b2a7b3a63177b830592c9ced9
astronuat <astronuat@vosges>
2022-04-04 01:25:08 +0800

    add entities for project,task and users

---
+++ src/main/java/com/accenture/codingtest/springbootcodingtest/entity/Project.java
@@ -0,0 +1,35 @@
+package com.accenture.codingtest.springbootcodingtest.entity;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "project")
+public class Project {
+
+	@Id
+	@Column(name = "id")
+	private String id;
+	@Column(name = "name", length = 250, unique = true)
+	private String name;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
