6eb8ee1a2f52708fa3f64846ac96dd472bbd53b9
amulya0512 <120387701+amulya0512@users.noreply.github.com>
2022-12-13 21:31:43 +0800

    Create entities

---
+++ Project.java
@@ -0,0 +1,50 @@
+package com.accenture.codingtest.springbootcodingtest.entity;
+
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+
+@Entity
+@Table(name="projects",
+uniqueConstraints={@UniqueConstraint(columnNames ={"name"})})
+public class Project {
+	@Id
+	@GeneratedValue(generator = "uuid2")
+	@GenericGenerator(name = "uuid2", strategy = "uuid2")
+	@Column(name = "id", updatable = false, nullable = false, columnDefinition = "VARCHAR(36)")
+	@Type(type = "uuid-char")
+	private UUID id;
+	@Column(name="name",nullable=false)
+	private String project_name;
+	
+	
+	public UUID getId() {
+		return id;
+	}
+	public void setId(UUID id) {
+		this.id = id;
+	}
+	public String getProject_name() {
+		return project_name;
+	}
+	public void setProject_name(String project_name) {
+		this.project_name = project_name;
+	}
+	@Override
+	public String toString() {
+		return "Project [id=" + id + ", project_name=" + project_name + "]";
+	}
+	
+	
+	
+
+}
