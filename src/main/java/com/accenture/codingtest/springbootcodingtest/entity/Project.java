89226b35d4bc838a5848f3bee3a1788f4edf674c
pirangicharan <ramcharan.psb@gmail.com>
2023-05-21 15:58:34 +0530

    Create entities

---
+++ src/main/java/com/accenture/codingtest/springbootcodingtest/entity/Project.java
@@ -0,0 +1,33 @@
+package com.accenture.codingtest.springbootcodingtest.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.UUID;
+
+@Entity
+@Table(name = "projects")
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    @Column(nullable = false,unique = true)
+    private String name;
+
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
